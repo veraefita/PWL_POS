14ba2164ecbc636b5e2dc0cd08c4137948d0a3d7
vera efita hudi putri <veraefita05@gmail.com>
2025-04-19 13:36:24 +0700

    js 4 prak 2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,8 +10,16 @@ class UserController extends Controller
 {
     public function index(): View
     {
-        $user = UserModel::where('level_id',2) ->count();
-        //dd($user);
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ]
+        );
+        $user->save();
+
         return view('user', ['data' => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,31 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data User</title>
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 200px;
-        }
-
-        th, td {
-            border: 1px solid black;
-            padding: 8px;
-            text-align: center;
-        }
-    </style>
 </head>
 <body>
     <h1>Data User</h1>
-    <table>
+    <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>Jumlah Pengguna</th>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>ID Level Pengguna</th>
         </tr>
+       
         <tr>
-            <td>{{ $data }}</td>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
         </tr>
+
     </table>
 </body>
 </html>

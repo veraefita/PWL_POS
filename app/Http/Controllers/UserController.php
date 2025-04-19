ddbbefc4c472fff0349e673a23687396359e3714
vera efita hudi putri <veraefita05@gmail.com>
2025-04-19 12:22:13 +0700

    js 4 prak 1 dan 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,31 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\View\Factory;
-use Illuminate\Contracts\View\View;
 
 class UserController extends Controller
 {
-    public function index(Factory $view): View
+    public function index(): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
     {
-        //tambah data user dengan Eloquent Model
-        /*$data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id' => 4,
-        ];
-
-        UserModel::insert($data); //tambahkan data ke tabel m_user*/
-
+        // tambah data user dengan Eloquent Model
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_dua',
+            'nama'     => 'Manager 2',
+            'password' => Hash::make('12345')
         ];
 
-        UserModel::where('username', 'customer-1')->update($data); //update data user
+        UserModel::create($data);
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
-        return $view->make('user', ['data' => $user]);
+        return view('user', ['data' => $user]);
     }
 }

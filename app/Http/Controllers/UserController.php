4347a0c7cc0111378e9f33e43f6e38da24c2adfa
vera efita hudi putri <veraefita05@gmail.com>
2025-04-21 20:33:28 +0700

    js 5 prak 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -126,8 +126,9 @@ class UserController extends Controller
         ];
 
         $activeMenu = 'user'; // set menu yang sedang aktif
+        $level = LevelModel::all(); // ambil data level untuk filter level
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page,'level' =>$level, 'activeMenu' => $activeMenu]);
     }
 
     // Ambil data user dalam bentuk JSON untuk DataTables
@@ -135,6 +136,11 @@ class UserController extends Controller
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
+
+        // Filter data user berdasarkan level_id
+if ($request->level_id) {
+    $users->where('level_id', $request->level_id);
+}
         
             return DataTables::of($users)
             // Menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)

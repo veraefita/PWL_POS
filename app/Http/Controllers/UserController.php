17603dc832d08de312d716807c8a4bdc6c127c7c
vera efita hudi putri <veraefita05@gmail.com>
2025-04-22 11:32:00 +0700

    js 6 prak 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -108,8 +108,11 @@ namespace App\Http\Controllers;
 
 use App\Models\LevelModel;
 use App\Models\UserModel;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Yajra\DataTables\DataTables;
+
 
 class UserController extends Controller
 {
@@ -282,7 +285,43 @@ public function destroy(string $id)
         return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
     }
 }
+public function create_ajax()
+{
+    $level = LevelModel::select('level_id', 'level_nama')->get();
 
+    return view('user.create_ajax')
+        ->with('level', $level);
+}
+public function store_ajax(Request $request)
+{
+    // cek apakah request berupa ajax
+    if ($request->ajax() || $request->wantsJson()) {
+        $rules = [
+            'level_id' => 'required|integer',
+            'username' => 'required|string|min:3|unique:m_user,username',
+            'nama'     => 'required|string|max:100',
+            'password' => 'required|min:6',
+        ];
+
+        // use Illuminate\Support\Facades\Validator;
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status'    => false, // response status, false: error/gagal, true: berhasil
+                'message'   => 'Validasi Gagal',
+                'msgField'  => $validator->errors(), // pesan error validasi
+            ]);
+        }
+
+        UserModel::create($request->all());
+        return response()->json([
+            'status'  => true,
+            'message' => 'Data user berhasil disimpan',
+        ]);
+    }
+    redirect('/');
+}
 }
 
 
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -5,44 +5,60 @@
         <h3 class="card-title">{{ $page->title }}</h3>
         <div class="card-tools">
             <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
+            <button onclick="modalAction('{{ url('/user/create_ajax,') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
         </div>
     </div>
     <div class="card-body">
-    @if (session('success'))
+        @if (session('success'))
         <div class="alert alert-success">{{ session('success') }}</div>
-    @endif
-    @if (session('error'))
+        @endif
+        @if (session('error'))
         <div class="alert alert-danger">{{ session('error') }}</div>
-    @endif
-    <div class="row">
-    <div class="col-md-12">
-        <div class="form-group row">
-            <label class="col-1 control-label col-form-label">Filter:</label>
-            <div class="col-3">
-                <select class="form-control" id="level_id" name="level_id" required>
-                    <option value="">- Semua -</option>
-                    @foreach($level as $item)
-                        <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
-                    @endforeach
-                </select>
-                <small class="form-text text-muted">Level Pengguna</small>
+        @endif
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Filter:</label>
+                    <div class="col-3">
+                        <select class="form-control" id="level_id" name="level_id" required>
+                            <option value="">- Semua -</option>
+                            @foreach($level as $item)
+                            <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                            @endforeach
+                        </select>
+                        <small class="form-text text-muted">Level Pengguna</small>
+                    </div>
+                </div>
             </div>
         </div>
+
+        <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Nama</th>
+                    <th>Level Pengguna</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+        </table>
     </div>
 </div>
-
-    <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
-        <thead>
-            <tr><th>ID</th><th>Username</th><th>Nama</th><th>Level Pengguna</th><th>Aksi</th></tr>
-        </thead>
-    </table>
-</div>
-</div>
+<!-- Modal -->
+<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog"
+    data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
 @push('css')
 @endpush
 @push('js')
 <script>
+        function modalAction(url = ''){
+        $('#myModal').load(url,function(){
+            $('#myModal').modal('show');
+         });
+    }
+    var dataUser;
     $(document).ready(function() {
         var dataUser = $('#table_user').DataTable({
             // serverSide: true, jika ingin menggunakan server side processing
@@ -51,7 +67,7 @@
                 "url": "{{ url('user/list') }}",
                 "dataType": "json",
                 "type": "POST",
-                "data": function (d) {
+                "data": function(d) {
                     d.level_id = $('#level_id').val();
                 }
             },
@@ -91,15 +107,13 @@
                 className: "",
                 orderable: false,
                 searchable: false
-            
-            }
-        ]
+
+            }]
         });
         $('#level_id').on('change', function() {
-    dataUser.ajax.reload();
-});
+            dataUser.ajax.reload();
+        });
 
-});
+    });
 </script>
 @endpush
-    
